9c982e00bbcb484e6844021ddb5967ebce38e1eb
Scott Kidder <scott.kidder@ilq.com>
2018-06-07 19:10:11 -0400

    lint

--- app/adapters/batch-request.js
+++ app/adapters/batch-request.js
@@ -3,6 +3,7 @@ import config from '../config/environment';
 
 export default BatchRequestAdapter.extend({
   host: config.apiURL,
+  /* eslint-disable ember/avoid-leaking-state-in-ember-objects */
   namespace: config.apiNamespace || '',
   apiBatchUrl: config.apiBatchUrl || '/batch'
 });
